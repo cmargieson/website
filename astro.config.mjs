0d17d034d427dd60ac80b042a23171c1cbcd5a36
cmargieson <cmargieson@gmail.com>
2026-05-26 12:26:06 +1000

    feat: add KaTeX support for rendering mathematical expressions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,21 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  site: "https://www.cmargieson.dev",
+  integrations: [
+    mdx({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
+    sitemap(),
+  ],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
