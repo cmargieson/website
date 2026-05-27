2b98ab79c0716f1284307c62a3ac6d569577f9e5
cmargieson <cmargieson@gmail.com>
2026-05-27 21:27:21 +1000

    feat: add optional tags field to blog post schema in content configuration

--- src/content.config.ts
+++ src/content.config.ts
@@ -12,6 +12,7 @@ const blog = defineCollection({
 			description: z.string(),
 			// Transform string to Date object
 			pubDate: z.coerce.date(),
+			tags: z.array(z.string()).optional(),
 		}),
 });
 
